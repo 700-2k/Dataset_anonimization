7ac5db769527360d96b9112bb5591f7a2b3dd334
700-2k <vl.v.skorodumov@gmail.com>
2025-10-22 05:16:05 +0300

    [src/anonimization.py] Added functions for anonimization direct identifiers and created example output

--- src/anonimization.py
+++ src/anonimization.py
@@ -13,5 +13,63 @@ def Load_table(path: str) -> pd.DataFrame:
     return table
 
 
+# ============================================
+# ✅ Data validation
+# ============================================
+
+
+def table_validate(table: pd.DataFrame) -> pd.DataFrame:
+    table["cards_number"] = table["cards_number"].astype(str)
+    return table
+
+
+# ============================================
+# 💾 Data export
+# ============================================
+
+
+def export_output(table: pd.DataFrame, path: str):
+    path = Path(path)
+    table.to_excel(path, index=False)
+
+
+# ============================================
+# 🔒 Anonimization functions
+# ============================================
+
+
+def anonymize_card_number(card: str) -> str:
+    card = "************" + str(card)[12:16]
+    return card
+
+
+methods = {"cards_number": anonymize_card_number}
+
+
+# ============================================
+# ⚙️ Data Processing
+# ============================================
+
+
+def anonymize_column(table: pd.DataFrame, column: str) -> pd.DataFrame:
+    table[column] = table[column].apply(methods[column])
+    return table
+
+
+def anonymize_direct_identifiers(table: pd.DataFrame) -> pd.DataFrame:
+    table = anonymize_column(table, "cards_number")
+    table = table.drop("receipt_id", axis=1)
+    return table
+
+
 if __name__ == "__main__":
-    path = "C:\\Users\\VLAD\\prog\\Dataset_anonimization\\data\\table.xlsx"
+    data_path = "C:\\Users\\VLAD\\prog\\Dataset_anonimization\\data\\table.xlsx"
+    out_path = "C:\\Users\\VLAD\\prog\\Dataset_anonimization\\output\\example.xlsx"
+
+    table = Load_table(data_path)
+
+    table = table_validate(table)
+
+    table = anonymize_direct_identifiers(table)
+
+    export_output(table, out_path)
